73b58a8b2abbeeabc5a8e55076b13e3b65fc43d8
djpn <davidjpn@icloud.com>
2019-07-23 22:44:25 -0600

    initial commit

--- app/src/main/java/edu/cnm/deepdive/abqtrailsclientside/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/abqtrailsclientside/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+//David Nelson put this here to commit.
 public class MainActivity extends AppCompatActivity {
 
   @Override
