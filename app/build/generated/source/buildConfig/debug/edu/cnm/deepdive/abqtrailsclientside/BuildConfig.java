828dd810d691abd0bf595d9dcce060d209ea9c5e
Denelle Britton Linebarger <brittod80@yahoo.com>
2019-07-29 15:28:30 -0600

    Added oauth to config.

--- app/build/generated/source/buildConfig/debug/edu/cnm/deepdive/abqtrailsclientside/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/edu/cnm/deepdive/abqtrailsclientside/BuildConfig.java
@@ -10,4 +10,8 @@ public final class BuildConfig {
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
+  // Fields from default config.
+  public static final String AUTHORIZATION_FORMAT = "Bearer %s";
+  public static final String BASE_URL = "http://10.0.2.2:28282/rest/abqtrails";
+  public static final String CLIENT_ID = "897420277761-lp06uk4jdv6k8hgo9er5mrbsobjt53ng.apps.googleusercontent.com";
 }
---
+++ app/src/main/java/edu/cnm/deepdive/abqtrailsclientside/service/GoogleSignInService.java
@@ -0,0 +1,53 @@
+package edu.cnm.deepdive.abqtrailsclientside.service;
+
+import android.app.Application;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import edu.cnm.deepdive.abqtrailsclientside.BuildConfig;
+
+public class GoogleSignInService {
+
+  private static Application context;
+
+  private GoogleSignInAccount account;
+  private GoogleSignInClient client;
+
+  private GoogleSignInService() {
+    GoogleSignInOptions options = new GoogleSignInOptions.Builder()
+        .requestEmail()
+        .requestId()
+        .requestProfile()
+        .requestIdToken(BuildConfig.CLIENT_ID)
+        .build();
+    client = GoogleSignIn.getClient(context, options);
+  }
+
+  public static void setContext(Application context) {
+    GoogleSignInService.context = context;
+  }
+
+  public GoogleSignInClient getClient() {
+    return client;
+  }
+
+  public GoogleSignInAccount getAccount() {
+    return account;
+  }
+
+  public void setAccount(GoogleSignInAccount account) {
+    this.account = account;
+  }
+
+  public static GoogleSignInService getInstance() {
+    return InstanceHolder.INSTANCE;
+  }
+
+  private static class InstanceHolder {
+
+    private static final GoogleSignInService INSTANCE = new GoogleSignInService();
+
+  }
+
+}
