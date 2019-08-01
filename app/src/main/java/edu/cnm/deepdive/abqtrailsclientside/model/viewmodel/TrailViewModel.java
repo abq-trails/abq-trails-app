b83a03493d74c4cbbfa3a8be9caaa367e733bf5c
djpn <davidjpn@icloud.com>
2019-08-01 11:46:35 -0600

    just to commit and push

--- app/src/main/java/edu/cnm/deepdive/abqtrailsclientside/model/viewmodel/TrailViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/abqtrailsclientside/model/viewmodel/TrailViewModel.java
@@ -8,17 +8,17 @@ import edu.cnm.deepdive.abqtrailsclientside.model.database.TrailsDatabase;
 import edu.cnm.deepdive.abqtrailsclientside.model.entity.Trail;
 import java.util.List;
 
-//public class TrailViewModel extends AndroidViewModel {
-//
-//  private LiveData<List<Trail>> trails;
-//
-//  public TrailViewModel(@NonNull Application application) {
-//    super(application);
-//  }
-//
-//  public LiveData<List<Trail>> getTrails() {
-//    trails = TrailsDatabase.getInstance(getApplication()).trailDao().getAll();
-//    return trails;
-//  }
-//
-//}
+public class TrailViewModel extends AndroidViewModel {
+
+  private LiveData<List<Trail>> trails;
+
+  public TrailViewModel(@NonNull Application application) {
+    super(application);
+  }
+
+  public LiveData<List<Trail>> getTrails() {
+    trails = TrailsDatabase.getInstance(getApplication()).trailDao().getAll();
+    return trails;
+  }
+
+}
