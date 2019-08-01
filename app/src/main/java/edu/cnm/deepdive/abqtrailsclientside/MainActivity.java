0d442f207a6e491d7fbc5ba0e5d7bb7f8ce31045
Anita <amartin98@cnm.edu.com>
2019-08-01 15:46:13 -0600

    ANITA - Configured the Google API Key.

--- app/src/main/java/edu/cnm/deepdive/abqtrailsclientside/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/abqtrailsclientside/MainActivity.java
@@ -2,19 +2,11 @@ package edu.cnm.deepdive.abqtrailsclientside;
 
 import android.content.Intent;
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
